b8aa511343af43052d81680ad4c42130d2cd4664
Ehtesham Ahmed <ehtexhamahmed2000@gmail.com>
2022-12-16 17:14:28 +0500

    projects

---
+++ project06_wordcount/bin/index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let totalWords = 0;
+async function count() {
+    let result = await inquirer.prompt([
+        {
+            type: 'text',
+            name: 'count',
+            message: "Enter the text: "
+        }
+    ]);
+    //For counting words
+    for (let i = 0; i < result.count.length; i++) {
+        if (result.count[i] === ' ') {
+            totalWords += 1;
+        }
+    }
+    totalWords += 1; // Since last word won't count in above block
+    //For counting characters
+    let totalCharacters = result.count.length;
+    for (let i = 0; i < result.count.length; i++) {
+        if (!(result.count[i] != ' ')) {
+            totalCharacters = totalCharacters - 1;
+        }
+    }
+    // if there is no text
+    if (result.count.length === 0) {
+        totalWords = 0;
+    }
+    console.log(`\nTotal Words: ${totalWords}`);
+    console.log(`\nTotal Characters (without whitespaces): ${totalCharacters}`);
+    console.log(`\nTotal Characters (with whitespaces): ${result.count.length}`);
+}
+await count();
